e21b46550d5f0ab07f73984d062d042b3b0e1086
gabguerin <gabriel.guerin.a@gmail.com>
2024-12-11 09:49:59 -0800

    Update frontend

--- backend/evaluation/evaluate.py
+++ backend/evaluation/evaluate.py
@@ -7,17 +7,14 @@ from backend.vectorstore import MultiModalVectorstore
 if __name__ == "__main__":
     rag_evaluator = RagEvaluator(student_llm=rag_model, db=MultiModalVectorstore())
 
-    evaluation_inputs = [
-        (
-            rag_evaluator.answer_accuracy_evaluator,
-            "answer-acc",
-        ),
-    ]
-    for metric_evaluator, description in evaluation_inputs:
+    evaluation_inputs = {
+        "answer_accuracy": rag_evaluator.answer_accuracy_evaluator,
+    }
+    for metric_id, metric_evaluator in evaluation_inputs.items():
         evaluate(
             rag_evaluator.run,
             data="OXFAM_dataset",
             evaluators=[metric_evaluator],
-            experiment_prefix=description,
+            experiment_prefix=metric_id,
             metadata={"version": "test"},
         )
